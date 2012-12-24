a97cb044465d85a9b0d703314eca58df86c86068
Skywodd <skywodd@gmail.com>
2012-12-24 17:59:23 +0100

    Fully re-implemented

--- playercontrols.cpp
+++ playercontrols.cpp
@@ -1,292 +1,314 @@
+/* Includes */
 #include <QMenu>
 #include <QCursor>
-#include <functional>
-
 #include "playercontrols.h"
 
-using namespace std::placeholders;
-
+/* Static members initialization */
 const QString PlayerControls::m_iconPlay(":/controls/play.png");
 const QString PlayerControls::m_iconPause(":/controls/pause.png");
 
 PlayerControls::PlayerControls(QWidget *parent) :
-    QWidget(parent), m_isplaying(false), m_fastLevel(100), m_playMode(QMediaPlaylist::Sequential), m_mode(BEGIN)
+    QWidget(parent), m_isplaying(false),
+    m_speedLevel(SPEED_NORMAL), m_playMode(QMediaPlaylist::Sequential),
+    m_mode(MODE_BEGIN)
 {
-
+    /* Setup UI */
     setupUi(this);
-    player_controls_previous->setEnabled(false);
+    setMode(MODE_BEGIN);
 
-    //Connect ui slots
-    connect(player_controls_play,&QAbstractButton::clicked, this, &PlayerControls::handlePlay);
-    connect(player_controls_stop,&QAbstractButton::clicked, this, &PlayerControls::handleStop);
-    connect(player_controls_next,&QAbstractButton::clicked, this, &PlayerControls::handleNext);
-    connect(player_controls_previous,&QAbstractButton::clicked, this, &PlayerControls::handlePrevious);
-    connect(player_controls_speed,&QAbstractButton::clicked, this, &PlayerControls::handleSpeed);
-    connect(player_controls_play_mode,&QAbstractButton::clicked, this, &PlayerControls::handlePlayMode);
+    /* Connect button signals to slots */
+    connect(player_controls_play, &QAbstractButton::clicked, this, &PlayerControls::setTogglePlayPause);
+    connect(player_controls_stop, &QAbstractButton::clicked, this, &PlayerControls::setStop);
+    connect(player_controls_next, &QAbstractButton::clicked, this, &PlayerControls::setNext);
+    connect(player_controls_previous, &QAbstractButton::clicked, this, &PlayerControls::setPrevious);
+    connect(player_controls_speed, &QAbstractButton::clicked, this, &PlayerControls::handleSpeedLevel);
+    connect(player_controls_play_mode, &QAbstractButton::clicked, this, &PlayerControls::handlePlayMode);
 }
 
 PlayerControls::~PlayerControls()
 {
-
 }
 
-// ---- Public slots
+void PlayerControls::setTogglePlayPause()
+{
+    /* toggle play / pause */
+    if (m_isplaying)
+        setPause();
+    else
+        setPlay();
+}
 
 void PlayerControls::setPlay()
 {
-    m_isplaying=true;
+    /* Force widget state to play */
+    m_isplaying = true;
     player_controls_play->setIcon(QIcon(m_iconPause));
     emit play();
-    if(m_mode==BEGIN)
-        setStatus(RUNNING);
 }
 
 void PlayerControls::setPause()
 {
-    m_isplaying=false;
+    /* Force widget state to pause */
+    m_isplaying = false;
     player_controls_play->setIcon(QIcon(m_iconPlay));
     emit pause();
 }
 
 void PlayerControls::setStop()
 {
-     handleStop();
-}
-
-void PlayerControls::setSpeed(int level)
-{
-    handleSpeedChanged(level);
-}
-
-void PlayerControls::setNext()
-{
-    handleNext();
-}
-
-void PlayerControls::setPrevious()
-{
-    handlePrevious();
-}
-
-void PlayerControls::setMode(Mode_t mode)
-{
-    handleMode(mode);
-}
-
-void PlayerControls::setPlayMode(QMediaPlaylist::PlaybackMode playMode){
-    if(playMode==QMediaPlaylist::Sequential)
-        handlePlayModeChangeSequential();
-    else if(playMode==QMediaPlaylist::Loop)
-        handlePlayModeChangeLoop();
-    else if(playMode==QMediaPlaylist::Random)
-        handlePlayModeChangeRandom();
-}
-
-void PlayerControls::setStatus(bool enabled)
-{
-    handleStatus(enabled);
-}
-
-void PlayerControls::reset()
-{
-    handleReset();
-}
-
-// ---- Private slots
-
-void PlayerControls::handlePlay()
-{
-    if( m_isplaying)
-        setPause();
-    else
-        setPlay();
-}
-
-void PlayerControls::handleStop()
-{
-    m_isplaying=false;
+    /* Force widget state to stop */
+    m_isplaying = false;
     player_controls_play->setIcon(QIcon(m_iconPlay));
     emit stop();
 }
 
-void PlayerControls::handleSpeed()
+void PlayerControls::setSpeed(Speed_t speed)
+{
+    /* Store speed modification level */
+    m_speedLevel = speed;
+
+    /* Emit speedChanged signal according factor */
+    switch(speed) {
+    case SPEED_025:
+        emit speedChanged(0.25);
+        break;
+
+    case SPEED_05:
+        emit speedChanged(0.5);
+        break;
+
+    case SPEED_NORMAL:
+        emit speedChanged(1);
+        break;
+
+    case SPEED_2:
+        emit speedChanged(2.0);
+        break;
+
+    case SPEED_4:
+        emit speedChanged(4.0);
+        break;
+    }
+}
+
+void PlayerControls::setPlayMode(QMediaPlaylist::PlaybackMode playMode){
+
+    /* Store playback mode */
+    m_playMode = playMode;
+
+    /* Emit the playModeChanged changed signal */
+    emit playModeChanged(playMode);
+}
+
+void PlayerControls::setNext()
+{
+    /* Emit next() signal */
+    if(m_mode != MODE_ENDING)
+        emit next();
+}
+
+void PlayerControls::setPrevious()
+{
+    /* Emit previous() signal */
+    if(m_mode != MODE_BEGIN)
+        emit previous();
+}
+
+void PlayerControls::setMode(Mode_t mode)
+{
+    /* Store mode */
+    m_mode = mode;
+
+    /* Apply display mode */
+    switch(mode) {
+    case MODE_BEGIN:
+        player_controls_previous->setEnabled(false);
+        player_controls_next->setEnabled(true);
+        break;
+
+    case MODE_RUNNING:
+        player_controls_previous->setEnabled(true);
+        player_controls_next->setEnabled(true);
+        break;
+
+    case MODE_ENDING:
+        player_controls_previous->setEnabled(true);
+        player_controls_next->setEnabled(false);
+        break;
+    }
+}
+
+void PlayerControls::setStatus(bool enabled)
+{
+    /* Set buttons states */
+    player_controls_play->setEnabled(enabled);
+    player_controls_stop->setEnabled(enabled);
+    player_controls_next->setEnabled(enabled);
+    player_controls_previous->setEnabled(enabled);
+    player_controls_speed->setEnabled(enabled);
+    player_controls_play_mode->setEnabled(enabled);
+
+    /* Restore display mode */
+    if(enabled)
+        setMode(m_mode);
+}
+
+void PlayerControls::reset()
+{
+    /* Reset widget to his default state */
+    setStop();
+    setPlayMode(QMediaPlaylist::Sequential);
+    setMode(MODE_BEGIN);
+    setStatus(false);
+}
+
+void PlayerControls::handleSpeedLevel()
 {
     QMenu changeSpeed(this);
-    QAction* level_1 = changeSpeed.addAction(tr("0.25"));
-    QAction* level_2 = changeSpeed.addAction(tr("0.5"));
-    QAction* level_3 = changeSpeed.addAction(tr("normal"));
-    QAction* level_4 = changeSpeed.addAction(tr("2"));
-    QAction* level_5 = changeSpeed.addAction(tr("4"));
 
+    /* Craft menu items */
+    QAction* level_1 = changeSpeed.addAction(tr("x0.25 (trés lent)"));
+    QAction* level_2 = changeSpeed.addAction(tr("x0.5 (lent)"));
+    QAction* level_3 = changeSpeed.addAction(tr("x1 (normal)"));
+    QAction* level_4 = changeSpeed.addAction(tr("x2 (rapide)"));
+    QAction* level_5 = changeSpeed.addAction(tr("x4 (trés rapide)"));
+
+    /* Set items checkable */
     level_1->setCheckable(true);
     level_2->setCheckable(true);
     level_3->setCheckable(true);
     level_4->setCheckable(true);
     level_5->setCheckable(true);
 
-    switch(m_fastLevel)
-    {
-    case 25:
+    /* Check the currently selected mode */
+    switch(m_speedLevel) {
+    case SPEED_025:
         level_1->setChecked(true);
         break;
-    case 50:
+    case SPEED_05:
         level_2->setChecked(true);
         break;
-    case 100:
+    case SPEED_NORMAL:
         level_3->setChecked(true);
         break;
-    case 200:
+    case SPEED_2:
         level_4->setChecked(true);
         break;
-    case 400:
+    case SPEED_4:
         level_5->setChecked(true);
         break;
     }
 
-    connect(level_1, &QAction::toggled, this, &PlayerControls::handleSpeedLevel1 );
-    connect(level_2, &QAction::toggled, this, &PlayerControls::handleSpeedLevel2 );
-    connect(level_3, &QAction::toggled, this, &PlayerControls::handleSpeedLevel3 );
-    connect(level_4, &QAction::toggled, this, &PlayerControls::handleSpeedLevel4 );
-    connect(level_5, &QAction::toggled, this, &PlayerControls::handleSpeedLevel5 );
+    /* Connect items signals to slots */
+    connect(level_1, &QAction::triggered, this, &PlayerControls::handleSpeedLevel1);
+    connect(level_2, &QAction::triggered, this, &PlayerControls::handleSpeedLevel2);
+    connect(level_3, &QAction::triggered, this, &PlayerControls::handleSpeedLevel3);
+    connect(level_4, &QAction::triggered, this, &PlayerControls::handleSpeedLevel4);
+    connect(level_5, &QAction::triggered, this, &PlayerControls::handleSpeedLevel5);
 
+    /* Show menu at the current cursor position */
     changeSpeed.exec(QCursor().pos());
+
+    /* Disconnect signals */
+    disconnect(level_1, &QAction::triggered, this, &PlayerControls::handleSpeedLevel1);
+    disconnect(level_2, &QAction::triggered, this, &PlayerControls::handleSpeedLevel2);
+    disconnect(level_3, &QAction::triggered, this, &PlayerControls::handleSpeedLevel3);
+    disconnect(level_4, &QAction::triggered, this, &PlayerControls::handleSpeedLevel4);
+    disconnect(level_5, &QAction::triggered, this, &PlayerControls::handleSpeedLevel5);
 }
 
 void PlayerControls::handleSpeedLevel1()
 {
-    m_fastLevel = 25;
-    emit speedChanged(25);
-
+    setSpeed(SPEED_025);
 }
 
 void PlayerControls::handleSpeedLevel2()
 {
-    m_fastLevel = 50;
-    emit speedChanged(50);
+    setSpeed(SPEED_05);
 }
 
 void PlayerControls::handleSpeedLevel3()
 {
-    m_fastLevel = 100;
-    emit speedChanged(100);
+    setSpeed(SPEED_NORMAL);
 }
 
 void PlayerControls::handleSpeedLevel4()
 {
-    m_fastLevel = 200;
-    emit speedChanged(200);
+    setSpeed(SPEED_2);
 }
-
 
 void PlayerControls::handleSpeedLevel5()
 {
-    m_fastLevel = 400;
-    emit speedChanged(400);
-}
-
-
-void PlayerControls::handleSpeedChanged(int level)
-{
-    m_fastLevel=level;
-    if(level<100)
-        emit fastRewind(level);
-    else
-        emit fastForward(level);
-}
-
-void PlayerControls::handleNext()
-{
-    emit next();
-    if(m_mode==BEGIN)
-        setStatus(RUNNING);
-}
-
-void PlayerControls::handlePrevious()
-{
-    emit previous();
-}
-
-void PlayerControls::handleStatus(bool enabled)
-{
-    player_controls_play->setEnabled(enabled);
-    player_controls_stop->setEnabled(enabled);
-
-    player_controls_next->setEnabled(enabled);
-    player_controls_previous->setEnabled(enabled);
-
-    player_controls_speed->setEnabled(enabled);
-
-    player_controls_play_mode->setEnabled(enabled);
-
-}
-
-void PlayerControls::handleReset()
-{
-    setStop();
-    setPlayMode(QMediaPlaylist::Sequential);
-    setMode(BEGIN);
-    setStatus(false);
+    setSpeed(SPEED_4);
 }
 
 void PlayerControls::handlePlayMode()
 {
     QMenu playMode(this);
-    QAction* sequential = playMode.addAction(tr("Lecture standard"));
-    QAction* loop = playMode.addAction(tr("Lecture en bouble"));
+
+    /* Craft menu items */
+    QAction* sequential = playMode.addAction(tr("Lecture normale"));
+    QAction* loopItem = playMode.addAction(tr("Lecture en bouble (morceau en cours)"));
+    QAction* loopPlaylist = playMode.addAction(tr("Lecture en bouble (playlist)"));
     QAction* random = playMode.addAction(tr("Lecture aléatoire"));
 
+    /* Set items checkable */
     sequential->setCheckable(true);
-    loop->setCheckable(true);
+    loopItem->setCheckable(true);
+    loopPlaylist->setCheckable(true);
     random->setCheckable(true);
 
-    if(m_playMode==QMediaPlaylist::Sequential)
+    /* Check the currently selected mode */
+    switch(m_playMode) {
+    case QMediaPlaylist::Sequential:
         sequential->setChecked(true);
-    else if(m_playMode==QMediaPlaylist::Loop)
-        loop->setChecked(true);
-    else if(m_playMode==QMediaPlaylist::Random)
+        break;
+
+    case QMediaPlaylist::CurrentItemInLoop:
+        loopItem->setChecked(true);
+        break;
+
+    case QMediaPlaylist::Loop:
+        loopPlaylist->setChecked(true);
+        break;
+
+    case QMediaPlaylist::Random:
         random->setChecked(true);
+        break;
+    }
 
-    connect(sequential, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeSequential );
-    connect(loop, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeLoop );
-    connect(random, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeRandom );
+    /* Connect items signals to slots */
+    connect(sequential, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeSequential);
+    connect(loopItem, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeLoopItem);
+    connect(loopPlaylist, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeLoopPlaylist);
+    connect(random, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeRandom);
 
+    /* Show menu at the current cursor position */
     playMode.exec(QCursor().pos());
+
+    /* Disconnect signals */
+    disconnect(sequential, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeSequential);
+    disconnect(loopItem, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeLoopItem);
+    disconnect(loopPlaylist, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeLoopPlaylist);
+    disconnect(random, &QAction::toggled, this, &PlayerControls::handlePlayModeChangeRandom);
 }
 
 void PlayerControls::handlePlayModeChangeSequential()
 {
-    m_playMode = QMediaPlaylist::Sequential;
-    emit playModeChanged(QMediaPlaylist::Sequential);
+    setPlayMode(QMediaPlaylist::Sequential);
 }
 
-void PlayerControls::handlePlayModeChangeLoop()
+void PlayerControls::handlePlayModeChangeLoopItem()
 {
-    m_playMode = QMediaPlaylist::Loop;
-    emit playModeChanged(QMediaPlaylist::Loop);
+   setPlayMode(QMediaPlaylist::CurrentItemInLoop);
+}
+
+void PlayerControls::handlePlayModeChangeLoopPlaylist()
+{
+    setPlayMode(QMediaPlaylist::Loop);
 }
 
 void PlayerControls::handlePlayModeChangeRandom()
 {
-    m_playMode = QMediaPlaylist::Random;
-    emit playModeChanged(QMediaPlaylist::Random);
-}
-
-void PlayerControls::handleMode(Mode_t mode)
-{
-    m_mode = mode;
-    switch (mode)
-    {
-    case BEGIN:
-        player_controls_previous->setEnabled(false);
-        emit modeChanged(mode);
-        break;
-    case RUNNING:
-        player_controls_previous->setEnabled(true);
-        emit modeChanged(mode);
-        break;
-    case ENDING:
-        player_controls_next->setEnabled(false);
-        emit modeChanged(mode);
-        break;
-    }
+    setPlayMode(QMediaPlaylist::Random);
 }
--- playercontrols.h
+++ playercontrols.h
@@ -1,81 +1,113 @@
 #ifndef PLAYERCONTROLS_H
 #define PLAYERCONTROLS_H
 
+/* Dependencies */
 #include "ui_playercontrols.h"
-
 #include <QMediaPlaylist>
 
+/**
+ * @brief Player controls's widget
+ *
+ * This class is designed to handle all controls of the media player.
+ */
 class PlayerControls : public QWidget, protected Ui::PlayerControls
 {
     Q_OBJECT
 
 public:
+    /**
+     * Controls modes
+     */
     typedef enum {
-        BEGIN,
-        RUNNING,
-        ENDING
+        MODE_BEGIN,
+        MODE_RUNNING,
+        MODE_ENDING
     } Mode_t;
 
+    /**
+     * Speed modification factor levels
+     */
+    typedef enum {
+        SPEED_025,
+        SPEED_05,
+        SPEED_NORMAL,
+        SPEED_2,
+        SPEED_4
+    } Speed_t;
+
+    /**
+     * Default constructor of the widget
+     *
+     * @brief PlayerControls
+     * @param parent Parent widget
+     */
     explicit PlayerControls(QWidget *parent = 0);
+
+    /**
+     * Default destructor
+     *
+     * @brief ~PlayerControls
+     * @remarks Ready for overload
+     */
     virtual ~PlayerControls();
 
 protected:
+    /** True if widget is in "play mode", false if in "pause mode" */
     bool m_isplaying;
-    int m_fastLevel;
+
+    /** Speed modification factor level */
+    Speed_t m_speedLevel;
+
+    /** Playback mode */
     QMediaPlaylist::PlaybackMode m_playMode;
-    Mode_t m_mode;
+
+    /** Controls display */
+    Mode_t m_mode; // NOTE usefull ?
+
+    /** Play icon ressource location */
     static const QString m_iconPlay;
+
+    /** Pause icon ressource location */
     static const QString m_iconPause;
 
-
 signals:
+    // TODO doxygen
     void play();
     void pause();
     void stop();
-    void fastRewind(int level);
-    void fastForward(int level);
     void next();
     void previous();
     void playModeChanged(QMediaPlaylist::PlaybackMode playMode);
-    void modeChanged(Mode_t mode);
-    void speedChanged(int speed);
-
+    void speedChanged(qreal speed);
 
 public slots:
+    void setTogglePlayPause();
     void setPlay();
     void setPause();
     void setStop();
-    void setSpeed(int level);
+    void setSpeed(Speed_t speed);
+    void setPlayMode(QMediaPlaylist::PlaybackMode playMode);
     void setNext();
     void setPrevious();
-
+    void setMode(Mode_t mode);
     void setStatus(bool enabled);
     void reset();
-    void setMode(Mode_t mode);
-    void setPlayMode(QMediaPlaylist::PlaybackMode playMode);
-
 
 protected slots:
-    void handlePlay();
-    void handleStop();
-    void handleSpeed();
+    // TODO use bind (if possible)
+    void handleSpeedLevel();
     void handleSpeedLevel1();
     void handleSpeedLevel2();
     void handleSpeedLevel3();
     void handleSpeedLevel4();
     void handleSpeedLevel5();
-    void handleSpeedChanged(int level);
-    void handleNext();
-    void handlePrevious();
-    void handleMode(Mode_t mode);
+
+    // TODO use bind (if possible)
     void handlePlayMode();
     void handlePlayModeChangeSequential();
-    void handlePlayModeChangeLoop();
+    void handlePlayModeChangeLoopItem();
+    void handlePlayModeChangeLoopPlaylist();
     void handlePlayModeChangeRandom();
-
-
-    void handleStatus(bool enabled);
-    void handleReset();
 };
 
 #endif // PLAYERCONTROLS_H
