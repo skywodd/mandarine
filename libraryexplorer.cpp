589c6599405bc48e2244532c7660470ac204fb85
Skywodd <skywodd@gmail.com>
2013-01-04 22:31:39 +0100

    TODO

--- libraryexplorer.cpp
+++ libraryexplorer.cpp
@@ -1,4 +1,5 @@
 /* Includes */
+#include <QContextMenuEvent>
 #include "libraryexplorer.h"
 
 LibraryExplorer::LibraryExplorer(QWidget *parent) :
@@ -6,8 +7,42 @@ LibraryExplorer::LibraryExplorer(QWidget *parent) :
 {
     /* Setup UI */
     setupUi(this);
+
+    /* Connect gui signals to slots */
+    connect(library_search_button, &QAbstractButton::clicked, this, &LibraryExplorer::handleReseach);
+    connect(library_search_input, &QLineEdit::returnPressed, this, &LibraryExplorer::handleReseach);
+    connect(library_explorer_displaymode, &QAbstractButton::clicked, this, &LibraryExplorer::handleDisplayMode);
+    connect(library_explorer_tree, &QTreeViewClickable::rightClicked, this, &LibraryExplorer::handleRightClick);
 }
 
 LibraryExplorer::~LibraryExplorer()
+{
+}
+
+void LibraryExplorer::handleReseach()
+{
+}
+
+void LibraryExplorer::handleRightClick()
+{
+}
+
+void LibraryExplorer::handleDisplayMode()
+{
+}
+
+void LibraryExplorer::handleDisplayModeMusic()
+{
+}
+
+void LibraryExplorer::handleDisplayModeAlbum()
+{
+}
+
+void LibraryExplorer::handleDisplayModeArtist()
+{
+}
+
+void LibraryExplorer::handleDisplayModeGenre()
 {
 }
--- libraryexplorer.h
+++ libraryexplorer.h
@@ -1,15 +1,56 @@
 #ifndef LIBRARYEXPLORER_H
 #define LIBRARYEXPLORER_H
 
+/* Dependencies */
 #include "ui_libraryexplorer.h"
 
+/**
+ * @brief LibraryExplorer display widget
+ *
+ * This class is designed as a QWidget for displaying library content.
+ * The widget internaly use a SQL database to store and retrieve data.
+ */
 class LibraryExplorer : public QWidget, protected Ui::LibraryExplorer
 {
     Q_OBJECT
     
 public:
+    /**
+     * Default construtor of the widget
+     *
+     * @brief LibraryExplorer
+     * @param parent Parent widget
+     */
     explicit LibraryExplorer(QWidget *parent = 0);
-    ~LibraryExplorer();
+
+    /**
+     * Default destructor of the widget
+     *
+     * @brief ~LibraryExplorer
+     * @remarks Ready for overload
+     */
+    virtual ~LibraryExplorer();
+
+    // setDatabaseLink(link)
+
+protected:
+    // m_databaseLink
+
+signals:
+    // addMediaToPlaylist()
+
+public slots:
+    // addFiles()
+    // addDirectories()
+
+protected slots:
+    void handleReseach();
+    void handleRightClick();
+    void handleDisplayMode();
+    void handleDisplayModeMusic();
+    void handleDisplayModeAlbum();
+    void handleDisplayModeArtist();
+    void handleDisplayModeGenre();
 };
 
 #endif // LIBRARYEXPLORER_H
