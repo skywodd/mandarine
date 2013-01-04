523bc46f80a51694633eaec13f00b84a163f556e
Skywodd <skywodd@gmail.com>
2013-01-04 19:43:02 +0100

    Finished

--- fileinformationsdialog.cpp
+++ fileinformationsdialog.cpp
@@ -1,36 +1,41 @@
 /* Includes */
+#include <QWidget>
+#include <QMediaResource>
 #include "fileinformationsdialog.h"
 
-FileInformationsDialog::FileInformationsDialog(QMediaResource *source, QWidget *parent) :
+FileInformationsDialog::FileInformationsDialog(QWidget* parent) :
     QDialog(parent)
 {
     /* Setup UI */
     setupUi(this);
-
-    /* Check if media is null */
-    if(source->isNull())
-        return;
-
-    /* Show ressource informations */
-    file_info_path->setText(source->url().toString());
-    file_info_bitrate->setText(QString("%1Kbps").arg(source->audioBitRate() / 8 / 1024));
-    file_info_codec->setText(source->audioCodec());
-    file_info_nb_channels->setText(QString("%1").arg(source->channelCount()));
-    qint64 size = source->dataSize();
-    if(size < 1024UL) {
-        file_info_size->setText(QString("%1o").arg(size));
-    } else if(size < 1048576UL) {
-        file_info_size->setText(QString("%1Kio").arg(size / 1024UL));
-    } else if(size < 1073741824UL) {
-        file_info_size->setText(QString("%1Mio").arg(size / 1048576UL));
-    } else {
-        file_info_size->setText(tr("gros CMB"));
-    }
-    file_info_sample_rate->setText(QString("%1KHz").arg(source->sampleRate() / 1000));
 }
 
 FileInformationsDialog::~FileInformationsDialog()
 {
+}
+
+void FileInformationsDialog::displayInfo(const QMediaResource *source)
+{
+    /* Check if media is null */
+    if(source->isNull())
+        return;
+
+    /* Display media informations */
+    file_info_path->setText(source->url().toString(QUrl::PrettyDecoded | QUrl::DecodeReserved));
+    file_info_bitrate->setText(QString("%1 Kbps").arg(source->audioBitRate()));
+    file_info_codec->setText(source->audioCodec());
+    file_info_nb_channels->setText(QString("%1").arg(source->channelCount()));
+    qint64 size = source->dataSize();
+    if(size < 1024UL) {
+        file_info_size->setText(QString("%1 o").arg(size));
+    } else if(size < 1048576UL) {
+        file_info_size->setText(QString("%1 Kio").arg((double) size / 1024UL, 0, 'f', 2));
+    } else if(size < 1073741824UL) {
+        file_info_size->setText(QString("%1 Mio").arg((double) size / 1048576UL, 0, 'f', 2));
+    } else {
+        file_info_size->setText(tr("gros CMB !"));
+    }
+    file_info_sample_rate->setText(QString("%1 Hz").arg(source->sampleRate()));
 }
 
 
--- fileinformationsdialog.h
+++ fileinformationsdialog.h
@@ -1,14 +1,45 @@
+/**
+ * @file fileinformationsdialog.h
+ * @brief Audio file's informations dialog box
+ * @author Fabien Batteix & Jordan Vertaure
+ * @version 1.0
+ *
+ * @section intro_sec Introduction
+ * This file contain all stuff required to instanciate FileInformationsDialog object.\n
+ * The FileInformationsDialog object is a dialog box with informations about audio file's properties.\n
+ * Informations like path, bitrate, codec, size are provided.\n
+ * \n
+ * Please report bug to <skywodd at gmail.com>
+ *
+ * @section license_sec License
+ *  This program is free software: you can redistribute it and/or modify\n
+ *  it under the terms of the GNU General Public License as published by\n
+ *  the Free Software Foundation, either version 3 of the License, or\n
+ *  (at your option) any later version.\n
+ * \n
+ *  This program is distributed in the hope that it will be useful,\n
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of\n
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n
+ *  GNU General Public License for more details.\n
+ * \n
+ *  You should have received a copy of the GNU General Public License\n
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.\n
+ */
 #ifndef FILEINFORMATIONSDIALOG_H
 #define FILEINFORMATIONSDIALOG_H
 
 /* Dependencies */
-#include <QMediaResource>
+#include <QDialog>
 #include "ui_fileinformationsdialog.h"
 
+/* Forward declarations */
+class QWidget;
+class QMediaResource;
+
 /**
- * @brief Audio file information display's dialog
+ * @brief Audio file's informations dialog
  *
- * This class is designed to display audio ressource informations.
+ * This class is designed as a QDialog extension for displaying audio file's properties.
  */
 class FileInformationsDialog : public QDialog, protected Ui::FileInformationsDialog
 {
@@ -16,20 +47,28 @@ class FileInformationsDialog : public QDialog, protected Ui::FileInformationsDia
     
 public:
     /**
-     * Default construcor of the dialog
+     * Default constructor of the dialog
      *
      * @brief FileInformationsDialog constructor
-     * @param source Source media ressource to display
      * @param parent Parent widget
      */
-    explicit FileInformationsDialog(QMediaResource *source, QWidget *parent = 0);
+    explicit FileInformationsDialog(QWidget* parent = 0);
 
     /**
      * Default destructor of the dialog
      *
      * @brief FileInformationsDialog destructor
+     * @remarks Ready for overload
      */
     virtual ~FileInformationsDialog();
+
+    /**
+     * Display informations about the specified QMediaResource object
+     *
+     * @brief displayInfo
+     * @param source Source QMediaResource pointer to display
+     */
+    void displayInfo(const QMediaResource* source);
 };
 
 #endif // FILEINFORMATIONSDIALOG_H
