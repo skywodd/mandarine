64685722938cdaf8746b36fe68a160d513a8793d
Skywodd <skywodd@gmail.com>
2013-01-04 15:43:52 +0100

    Add comment & cleanup

--- qtableviewclickable.cpp
+++ qtableviewclickable.cpp
@@ -1,22 +1,23 @@
-#include <QDebug>
-#include <QMenu>
-#include <QModelIndex>
+/* Includes */
 #include <QContextMenuEvent>
-
 #include "qtableviewclickable.h"
 
 QTableViewClickable::QTableViewClickable(QWidget *parent) :
     QTableView(parent)
 {
-
 }
 
-
+QTableViewClickable::~QTableViewClickable()
+{
+}
 
 void QTableViewClickable::contextMenuEvent(QContextMenuEvent* event)
 {
+    /* Emit signal for rigth click */
+    emit rightClicked(event);
 
-        emit rightClicked(event);
+    /* Resend event to parent */
+    QTableView::contextMenuEvent(event);
 }
 
 
--- qtableviewclickable.h
+++ qtableviewclickable.h
@@ -4,25 +4,49 @@
 /* Dependencies */
 #include <QTableView>
 
+/**
+ * @brief QTableView with rigth click support
+ *
+ * This class is a enhanced version of QTableView widget with rigth click support
+ */
 class QTableViewClickable : public QTableView
 {
     Q_OBJECT
+
 public:
-    explicit QTableViewClickable(QWidget *parent = 0);
+    /**
+     * Default constructor of the widget
+     *
+     * @brief QTableViewClickable constructor
+     * @param parent Parent widget
+     */
+    explicit QTableViewClickable(QWidget* parent = 0);
+
+    /**
+     * Default destructor of the widget
+     *
+     * @brief QTableViewClickable destructor
+     * @remarks Ready for overload
+     */
+    virtual ~QTableViewClickable();
     
 signals:
+    /**
+     * Signal emitted when user rigth click in the widget
+     *
+     * @brief rightClicked
+     * @param event Requested QContextMenuEvent event
+     */
     void rightClicked(QContextMenuEvent* event);
-public slots:
-    
-protected :
 
+protected:
+    /**
+     * Overload of the default QTableView contextMenuEvent() for rigth click support
+     *
+     * @brief contextMenuEvent
+     * @param event Requested QContextMenuEvent event
+     */
     void contextMenuEvent(QContextMenuEvent* event);
-
-    void handleRightClicked();
-
-    void handleContextualMenuPlay();
-
-    void handleContextualMenuRemove();
 };
 
 #endif // QTABLEVIEWCLICKABLE_H
