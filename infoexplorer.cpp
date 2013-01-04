b554eda1051514b6d98d7d1d713d60e093e40d90
Skywodd <skywodd@gmail.com>
2013-01-04 19:53:28 +0100

    Finished

--- infoexplorer.cpp
+++ infoexplorer.cpp
@@ -1,3 +1,20 @@
+/*
+ * See header file for details
+ *
+ *  This program is free software: you can redistribute it and/or modify\n
+ *  it under the terms of the GNU General Public License as published by\n
+ *  the Free Software Foundation, either version 3 of the License, or\n
+ *  (at your option) any later version.\n
+ *
+ *  This program is distributed in the hope that it will be useful,\n
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of\n
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n
+ *  GNU General Public License for more details.\n
+ *
+ *  You should have received a copy of the GNU General Public License\n
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.\n
+ */
+
 /* Includes */
 #include <QUrl>
 #include "infoexplorer.h"
@@ -13,8 +30,8 @@ InfoExplorer::~InfoExplorer()
 {
 }
 
-void InfoExplorer::showInformation(const QString &album)
+void InfoExplorer::showInformation(const QString& terms)
 {
-    /* Open the mobile version of the Wikipedia page for the specified search term */
-    info_explorer_webview->load(QUrl(QString("http://en.m.wikipedia.org/wiki/Main_Page/?search=%1").arg(album)));
+    /* Open the mobile version of the Wikipedia page for the specified search terms */
+    info_explorer_webview->load(QUrl(QString("http://en.m.wikipedia.org/wiki/Main_Page/?search=%1").arg(terms)));
 }
--- infoexplorer.h
+++ infoexplorer.h
@@ -1,11 +1,39 @@
+/**
+ * @file infoexplorer.h
+ * @brief Wikipedia informations widget
+ * @author Fabien Batteix & Jordan Vertaure
+ * @version 1.0
+ *
+ * @section intro_sec Introduction
+ * This file contain all stuff required to instantiate InfoExplorer object.\n
+ * The InfoExplorer object is designed to display wikipedia informations about the specified terms.\n
+ * Informations are displayed in WebKit as mobile view.\n
+ * \n
+ * Please report bug to <skywodd at gmail.com>
+ *
+ * @section license_sec License
+ *  This program is free software: you can redistribute it and/or modify\n
+ *  it under the terms of the GNU General Public License as published by\n
+ *  the Free Software Foundation, either version 3 of the License, or\n
+ *  (at your option) any later version.\n
+ * \n
+ *  This program is distributed in the hope that it will be useful,\n
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of\n
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n
+ *  GNU General Public License for more details.\n
+ * \n
+ *  You should have received a copy of the GNU General Public License\n
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.\n
+ */
 #ifndef INFOEXPLORER_H
 #define INFOEXPLORER_H
 
 /* Dependencies */
+#include <QWidget>
 #include "ui_infoexplorer.h"
 
 /**
- * @brief Wikipedia informations display widget
+ * @brief Wikipedia display widget
  *
  * This widget is designed to display wikipedia informations about album or authors.
  */
@@ -17,7 +45,7 @@ public:
     /**
      * Default constructor of the widget
      *
-     * @brief InfoExplorer
+     * @brief InfoExplorer constructor
      * @param parent Parent widget
      */
     explicit InfoExplorer(QWidget *parent = 0);
@@ -25,19 +53,18 @@ public:
     /**
      * Default destructor of the widget
      *
-     * @brief ~InfoExplorer
+     * @brief InfoExplorer destructor
      * @remarks Ready for overload
      */
     virtual ~InfoExplorer();
 
     /**
-     * Display available informations about the specified album
+     * Display available informations about the specified terms
      *
      * @brief showInformation
-     * @param album The album or author(s) name to search and display
+     * @param terms The terms to search on wikipedia and display
      */
-    void showInformation(const QString& album);
-
+    void showInformation(const QString& terms);
 };
 
 #endif // INFOEXPLORER_H
